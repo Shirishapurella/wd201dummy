52441c8ab41bae3f03e91c23b02ae22f37a800cb
Shirishapurella <151667861+Shirishapurella@users.noreply.github.com>
2023-12-22 19:10:58 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,52 +1,63 @@
-const http = require("http");
-const fs = require("fs");
-const argv = require("minimist")(process.argv.slice(2));
+//Reading command line arguments
+const args = require("minimist")(process.argv.slice(2),{
+  alias:{
+    p: "port",
+  },
+});
 
+console.log(args);
+
+//Required port
+let reqPort = args.p;
+
+const http = require("http");
+const fs  = require("fs");
 
 let homeContent = "";
 let projectContent = "";
-let registrationContent ="";
+let registrationContent = "";
+
 
 fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  homeContent = home;
+    if(err){
+        throw err;
+    }
+    homeContent = home;
 });
 
-fs.readFile("project.html", (err, project) => {
-  if (err) {
+fs.readFile("project.html",(err,project) => {
+  if(err){
     throw err;
   }
   projectContent = project;
 });
 
-fs.readFile("registration.html", (err, registration) => {
-  if (err) {
+fs.readFile("registration.html",(err,registration) => {
+  if(err){
     throw err;
   }
   registrationContent = registration;
 });
 
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      case "/registration":
-        response.write(registrationContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  }) 
-  .listen(argv.port || 3000,()=>{
-    console.log(`running on ${argv.port || 3000}`);
-  });
+
+http.createServer((request, response) => {
+  let url = request.url;
+  response.writeHeader(200, { "Content-Type": "text/html" });
+  switch (url) {
+    case "/project":
+      response.write(projectContent);
+      response.end();
+      break;
+
+    //New route..
+    case "/registration":
+      response.write(registrationContent);
+      response.end();
+      break;
+
+    default:
+      response.write(homeContent);
+      response.end();
+      break;
+  }
+}).listen(reqPort);
